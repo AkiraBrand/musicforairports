143a9a8f95aef5d6970c072c240d57ba44c71b60
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-06-10 23:14:04 -0700

    adds pseducode sample library and fetchSample method

---
+++ musicforairports.js
@@ -0,0 +1,13 @@
+//get a sample of an instrument
+//
+
+const SAMPLE_LIBRARY = {
+  //put an data structure here with the instrument, what notes are availabe,
+  //and where in the file structure they can be found
+}
+
+
+function getSample(instrument, noteAndOctave) {
+  //search the sample library for the instruement
+  //and the closest note and octave to the input
+}
