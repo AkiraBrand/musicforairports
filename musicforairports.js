f2f75a0f2067e897c05a2ce9c467c5808648594b
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-07-16 20:49:04 -0700

    adds javascript sample object

--- musicforairports.js
+++ musicforairports.js
@@ -1,29 +1,20 @@
-// //get a sample of an instrument
-// //
-//
-// const SAMPLE_LIBRARY = {
-//   //put an data structure here with the instrument, what notes are availabe,
-//   //and where in the file structure they can be found
-// }
-//
-// const OCTAVE = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
-//
-// ]
-//
-//
-// function getSample(instrument, noteAndOctave) {
-//   //search the sample library for the instruement
-//   //and the closest note and octave to the input
-//   //
-//   //take noteAndOctave and split them into two sections with
-//   //regex or something else
-//   //
-//   //run octave through parseInt(requestedOctave, 10)
-//   //run the notes thorugh flatToSharp method
-//   //
-// }
-//
-// function flatToSharp(note){
-//   //write a switch(note) function that takes
-//   //flat notes as a case and returns the enharmonic sharp
-// }
+const SAMPLE_LIBRARY =  {
+  'Grand Piano': [
+      { note: 'A', octave: 4, file: 'Samples/Grand Piano/piano-f-a4.wav' },
+      { note: 'A', octave: 5, file: 'Samples/Grand Piano/piano-f-a5.wav' },
+      { note: 'A', octave: 6, file: 'Samples/Grand Piano/piano-f-a6.wav' },
+      { note: 'C', octave: 4, file: 'Samples/Grand Piano/piano-f-c4.wav' },
+      { note: 'C', octave: 5, file: 'Samples/Grand Piano/piano-f-c5.wav' },
+      { note: 'C', octave: 6, file: 'Samples/Grand Piano/piano-f-c6.wav '},
+      { note: 'D#', octave: 4, file: 'Samples/Grand Piano/piano-f-d#4.wav' },
+      { note: 'D#', octave: 5, file: 'Samples/Grand Piano/piano-f-d#5.wav' },
+      { note: 'D#', octave: 6, file: 'Samples/Grand Piano/piano-f-d#6.wav' },
+      { note: 'F#', octave: 4, file: 'Samples/Grand Piano/piano-f-f#4.wav' },
+      { note: 'F#', octave: 5, file: 'Samples/Grand Piano/piano-f-f#5.wav' },
+      { note: 'F#', octave: 6, file: 'Samples/Grand Piano/piano-f-f#6.wav' },
+  ]
+}
+
+function getSample (instrument, noteAndOctave) {
+
+}
