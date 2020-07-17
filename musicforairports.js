ff71b60ed1b1d4cc2421699f4fe011fe998a00f1
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-07-16 21:00:37 -0700

    adds enharmonic function

--- musicforairports.js
+++ musicforairports.js
@@ -19,3 +19,14 @@ function getSample (instrument, noteAndOctave) {
     let[, requestedNote, requestedOctave] = /^(\w[b#]?)(\d)$/.exec(noteAndOctave);
     requestedOctave = parseInt(requestedOctave, 10);
 }
+
+function flatToSharp (note) {
+  switch (note) {
+      case 'Bb': return 'A#';
+      case 'Db': return 'C#';
+      case 'Eb': return 'D#';
+      case 'Gb': return 'F#';
+      case 'Ab': return 'G#';
+      default: return note;
+  }
+}
