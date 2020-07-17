b5952501339aaad3b67d34054aaa815df3976d42
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-07-16 20:33:35 -0700

    Adds grand piano sampler

--- musicforairports.js
+++ musicforairports.js
@@ -1,29 +1,29 @@
-//get a sample of an instrument
+// //get a sample of an instrument
+// //
 //
-
-const SAMPLE_LIBRARY = {
-  //put an data structure here with the instrument, what notes are availabe,
-  //and where in the file structure they can be found
-}
-
-const OCTAVE = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
-
-]
-
-
-function getSample(instrument, noteAndOctave) {
-  //search the sample library for the instruement
-  //and the closest note and octave to the input
-  //
-  //take noteAndOctave and split them into two sections with
-  //regex or something else
-  //
-  //run octave through parseInt(requestedOctave, 10)
-  //run the notes thorugh flatToSharp method
-  //
-}
-
-function flatToSharp(note){
-  //write a switch(note) function that takes
-  //flat notes as a case and returns the enharmonic sharp
-}
+// const SAMPLE_LIBRARY = {
+//   //put an data structure here with the instrument, what notes are availabe,
+//   //and where in the file structure they can be found
+// }
+//
+// const OCTAVE = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+//
+// ]
+//
+//
+// function getSample(instrument, noteAndOctave) {
+//   //search the sample library for the instruement
+//   //and the closest note and octave to the input
+//   //
+//   //take noteAndOctave and split them into two sections with
+//   //regex or something else
+//   //
+//   //run octave through parseInt(requestedOctave, 10)
+//   //run the notes thorugh flatToSharp method
+//   //
+// }
+//
+// function flatToSharp(note){
+//   //write a switch(note) function that takes
+//   //flat notes as a case and returns the enharmonic sharp
+// }
